41f2f8f982fe958220275ec8bda782ff916ef8ce
AvivaMagame <tesheva.kristina@yandex.ru>
2023-10-04 18:01:22 +0500

    Изменени

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ 
+  <div id="app">
+   <HelloWorld/>
+
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+
+
 export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+   
+
+  } 
 }
 </script>
 
